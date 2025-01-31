1bc6d65fd8c05ebc89a82c00c44339624f1bb131
llenoid <brandon1236perez@gmail.com>
2025-01-31 19:18:05 +0800

    connected app to mongo database

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const path = require('path')
+const dotenv = require('dotenv')
+const {MongoClient} = require('mongodb')
+const Joi = require('joi')
+const fs = require('fs')
+
+dotenv.config()
 
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(cors())
+
+app.use(morgan('dev'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const dbUri = process.env.DB_URI
+MongoClient.connect(dbUri, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(client=> {
+    const db = client.db('myDb')
+    const userCollection = db.collection('users')
+  })
+  .catch(err=> {
+    console.error('Failed to connect to the database',err)
+  })
 
 app.get('/users',async(req,res)=> {
   // just testing out endpoint using postman
