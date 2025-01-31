9e1e8c231fec8b79016a8d2537ae72dd533df58f
llenoid <brandon1236perez@gmail.com>
2025-01-31 18:59:10 +0800

    added mock api endpoints to test if they work in postman

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express')
 
 const app = express()
 
+app.get('/users',async(req,res)=> {
+  // just testing out endpoint using postman
+  res.send('get users')
+  console.log('get users')
+})
+
+app.post('/users', async(req,res)=> {
+  res.send('add new user')
+})
+
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server running on port ${process.env.PORT || 3000}`)
 })
